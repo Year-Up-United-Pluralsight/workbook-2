cf52869e9b66d070e3539e4d3beb2285fd81ff4b
dinganic03 <dinganic03@gmail.com>
2025-04-18 13:11:55 -0400

    Added person class

--- ClassDemo1/src/main/java/com/pluralsight/Main.java
+++ ClassDemo1/src/main/java/com/pluralsight/Main.java
@@ -1,30 +1,61 @@
 package com.pluralsight;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
 
+        String fullname;
+        int age;
+        String profession;
 
-
+        // Get person info from user
         System.out.print("What is the person's full name? ");
-        String fullname = scanner.nextLine();
+        fullname = scanner.nextLine();
 
         System.out.print("What is the person's age? ");
-        int age = scanner.nextInt();
+        age = scanner.nextInt();
         scanner.nextLine();  // clears the CRLF
 
         System.out.print("What is the person's profession? ");
-        String profession = scanner.nextLine();
+        profession = scanner.nextLine();
 
-        displayPerson(fullname, age, profession);
-        savePerson(fullname, age, profession);
+        // Create a person
+        Person thePerson = new Person (fullname, age, profession);
+
+        // Get person info from user
+        System.out.print("What is the person's full name? ");
+        fullname = scanner.nextLine();
+
+        System.out.print("What is the person's age? ");
+        age = scanner.nextInt();
+        scanner.nextLine();  // clears the CRLF
+
+        System.out.print("What is the person's profession? ");
+        profession = scanner.nextLine();
+
+        // Create a person
+        Person theOtherPerson = new Person (fullname, age, profession);
+
+
+        // Displays person's fullname using class method
+        System.out.println(thePerson.getFullname());
+
+        System.out.println(theOtherPerson.getFullname());
+
+
+        displayPerson(thePerson);
+        displayPerson(theOtherPerson);
 
     }
 
 
-    public static void displayPerson (String fullname, int age, String profession){
-        System.out.printf("Person %s is a %s and is %d years old.", fullname, profession, age);
+    public static void displayPerson (Person person){
+        System.out.printf("Person %s is a %s and is %d years old.\n",
+                person.getFullname(),
+                person.getProfession(),
+                person.getAge());
 
     }
 
